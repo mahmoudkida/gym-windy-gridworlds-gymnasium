60c57ef52bee9719180bbdc1aa12b04b026a18cd
Mahmoud Kida <mramadan6@gatech.edu>
2024-04-09 17:12:38 +0300

    Update __init__.py
    update to register for gymnasium

--- gym_windy_gridworlds/__init__.py
+++ gym_windy_gridworlds/__init__.py
@@ -1,19 +1,19 @@
-from gym.envs.registration import register
+import gymnasium as gym
+from gymnasium.envs.registration import register
 
 register(
-        id='WindyGridWorld-v0',
-        entry_point='gym_windy_gridworlds.envs:WindyGridWorldEnv',
-        )
+    id='WindyGridWorld-v0',
+    entry_point='gym_windy_gridworlds.envs:WindyGridWorldEnv',
+)
 register(
-        id='StochWindyGridWorld-v0',
-        entry_point='gym_windy_gridworlds.envs:StochWindyGridWorldEnv',
-        )
+    id='StochWindyGridWorld-v0',
+    entry_point='gym_windy_gridworlds.envs:StochWindyGridWorldEnv',
+)
 register(
-        id='KingWindyGridWorld-v0',
-        entry_point='gym_windy_gridworlds.envs:KingWindyGridWorldEnv',
-        )		
+    id='KingWindyGridWorld-v0',
+    entry_point='gym_windy_gridworlds.envs:KingWindyGridWorldEnv',
+)
 register(
-        id='StochKingWindyGridWorld-v0',
-        entry_point='gym_windy_gridworlds.envs:StochKingWindyGridWorldEnv',
-        )
-		
+    id='StochKingWindyGridWorld-v0',
+    entry_point='gym_windy_gridworlds.envs:StochKingWindyGridWorldEnv',
+)
